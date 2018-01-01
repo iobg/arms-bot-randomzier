0aab496892bd3bc14ce405d39317e98b9efa8b80
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2018-01-01 11:05:10 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,38 @@
+var Discord = require('discord.js');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+
+var bot = new Discord.Client();
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (message) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    var text= message.content;
+    if (text.substring(0, 1) == '!') {
+        var args = text.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                message.channel.send('pong');
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
+bot.login(auth.token);
+
